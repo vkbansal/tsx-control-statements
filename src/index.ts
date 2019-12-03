b7e834e85404eafbe9d60f6e6ac2a5a22fffd08a
Vivek Kumar Bansal <vkb0310@gmail.com>
2019-12-03 21:49:58 +0530

    chore: add index.ts file

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export {
+  If,
+  IfProps,
+  For,
+  ForProps,
+  Choose,
+  ChooseProps,
+  When,
+  WhenProps,
+  Otherwise,
+  OtherwiseProps
+} from './components';
+
+export { default as transformer } from './transformer';
